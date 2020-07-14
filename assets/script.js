1064a2a3b39884db724fd7f39f9b43981b9ab819
xtreme6007 <prestonnichols96@gmail.com>
2020-07-14 11:07:20 -0500

    added variables

--- assets/script.js
+++ assets/script.js
@@ -3,12 +3,32 @@ var generateBtn = document.querySelector("#generate");
 
 // Write password to the #password input
 function writePassword() {
+  // Password criteria
+  var charLength = prompt("How many characters? Must be between 8 - 128 characters");  
+  var special = confirm("Click OK to use special character's!");
+  var lower = confirm("Click OK to you use lower case characters!");
+  ‹var upper = confirm("Click OK to use uppercase characters!");
+  var numeric = confirm("Click OK to use numeric characters!"); 
+
+
+  var lowercase = "abcdefghijklmnopqrstuvwxyz".split();
+  var num = "0123456789".split
+  var special = "!@#$%^&*()_+{}|[]\';:.,></?".split
+var upper = lowercase.toUpperCase().split("");
+
   var password = generatePassword();
   var passwordText = document.querySelector("#password");
-
+  
   passwordText.value = password;
+  
+  }
 
-}
 
 // Add event listener to generate button
 generateBtn.addEventListener("click", writePassword);
+
+
+// Criteria
+
+//document.getElementByIdgenerate
+
