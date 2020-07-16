91fddbeb4f76bdecfcf23d33c072be1a5dabde0c
xtreme6007 <prestonnichols96@gmail.com>
2020-07-15 22:11:13 -0500

    adding functions

--- assets/script.js
+++ assets/script.js
@@ -4,18 +4,7 @@ var generateBtn = document.querySelector("#generate");
 // Write password to the #password input
 function writePassword() {
   // Password criteria
- 
-  var lowerCase = "abcdefghijklmnopqrstuvwxyz".split("");
-  var num = "0123456789".split("");
-  var specialChar = "!@#$%^&*()_+{}|[]\';:.,></?".split("");
-  var upperCase = "ABCDEFGHIJKLMNOPQRSTUVWXYZ".split("");
 
-
-  var charLength = prompt("How many characters? Must be between 8 - 128 characters");
-  var special = confirm("Click OK to use special character's!");
-  var lower = confirm("Click OK to you use lower case characters!");
-  var upper = confirm("Click OK to use uppercase characters!");
-  var numeric = confirm("Click OK to use numeric characters!");
 
   var password = generatePassword();
   var passwordText = document.querySelector("#password");
@@ -24,36 +13,53 @@ function writePassword() {
 
 
   function generatePassword() {
-    var characterArray = [];
+    var characterArray = "";
     var password = "";
 
+    var lowerCase = "abcdefghijklmnopqrstuvwxyz".split("");
+    var num = "0123456789".split("");
+    var specialChar = "!@#$%^&*()_+{}|[]\';:.,></?";
+    var upperCase = "ABCDEFGHIJKLMNOPQRSTUVWXYZ".split("");
+
+    var charLength = prompt("How many characters? Must be between 8 - 128 characters");
+    if (charLength < 8 || charLength > 128) {
+      alert("Please Enter a vaild length");
+      writePassword();
+    } else {
+    var special = confirm("Click OK to use special character's!");
+    var lower = confirm("Click OK to you use lower case characters!");
+    var upper = confirm("Click OK to use uppercase characters!");
+    var numeric = confirm("Click OK to use numeric characters!");
+
     if (special) {
-      characterArray.concat(specialChar);
+      characterArray = characterArray.concat(specialChar);
+      
     }
     if (lower) {
-      characterArray.concat(lowerCase);
+      characterArray = characterArray.concat(lowerCase);
+      
     }
     if (upper) {
-      characterArray.concat(upperCase);
+      characterArray = characterArray.concat(upperCase);
     }
     if (numeric) {
-      characterArray.concat(num)
+      characterArray = characterArray.concat(num);
     }
     for (var i = 0; i < charLength; i++) {
       var randomNumber = Math.floor(Math.random() * characterArray.length);
       var character = characterArray[randomNumber];
       password += character;
     }
-      return password;
+    return password;
 
   }
-
+}
 }
 // Add event listener to generate button
 generateBtn.addEventListener("click", writePassword);
 
 
-// Criteria
 
-//document.getElementByIdgenerate
+
+
 
