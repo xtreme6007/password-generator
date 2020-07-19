f70f4e3d8d97f32ad3303a2b65941cc4a8ce7bef
xtreme6007 <prestonnichols96@gmail.com>
2020-07-19 17:17:21 -0500

    final updates added if statement if no options are selected function exits

--- assets/script.js
+++ assets/script.js
@@ -22,15 +22,15 @@ function writePassword() {
     var upperCase = "ABCDEFGHIJKLMNOPQRSTUVWXYZ"
 
     var charLength = prompt("How many characters? Must be between 8 - 128 characters");
-    if(charLength < 8 || charLength > 128) {
+    if (charLength < 8 || charLength > 128) {
       var charLength = prompt("Please enter a number between 8 - 128");
-      if (charLength < 8 || charLength > 128){
-        exit; 
-      } 
-      
-    } 
+      if (charLength < 8 || charLength > 128) {
+        exit;
+      }
 
-   
+    }
+
+
     var special = confirm("Click OK to use special character's!");
     var lower = confirm("Click OK to you use lower case characters!");
     var upper = confirm("Click OK to use uppercase characters!");
@@ -38,13 +38,10 @@ function writePassword() {
 
     if (special) {
       characterArray = characterArray.concat(specialChar);
-      
     }
-      
-    
     if (lower) {
       characterArray = characterArray.concat(lowerCase);
-      
+
     }
     if (upper) {
       characterArray = characterArray.concat(upperCase);
@@ -52,23 +49,21 @@ function writePassword() {
     if (numeric) {
       characterArray = characterArray.concat(num);
     }
+    if (special !== true && lower !== true && upper !== true && numeric !== true) {
+      alert("you must choose one option");
+      exit;
+    }
     for (var i = 0; i < charLength; i++) {
       var randomNumber = Math.floor(Math.random() * characterArray.length);
       var character = characterArray[randomNumber];
       password += character;
     }
-  
+
     return password;
-  
-    
-}
+
+
+  }
 
 }
 // Add event listener to generate button
 generateBtn.addEventListener("click", writePassword);
-
-
-
-
-
-
